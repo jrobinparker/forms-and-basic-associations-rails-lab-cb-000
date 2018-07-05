b22a60f603625c22e49a51d3a722cabefc66c9be
Jeremy Parker <jrobinparker@gmail.com>
2018-07-05 21:37:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114013240) do
+ActiveRecord::Schema.define(version: 20180705213600) do
 
   create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "artist_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string   "name"
+    t.string   "genre_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
