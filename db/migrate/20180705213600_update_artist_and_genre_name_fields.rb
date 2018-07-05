c1f660fccd0a818048773aff10f0d39a18ae71eb
Jeremy Parker <jrobinparker@gmail.com>
2018-07-05 21:36:45 +0000

    automatically backed up by learn

--- db/migrate/20180705213600_update_artist_and_genre_name_fields.rb
+++ db/migrate/20180705213600_update_artist_and_genre_name_fields.rb
@@ -1,5 +1,6 @@
 class UpdateArtistAndGenreNameFields < ActiveRecord::Migration
   def change
     rename_column :artists, :name, :artist_name
+    rename_column :genres, :name, :genre_name
   end
 end
